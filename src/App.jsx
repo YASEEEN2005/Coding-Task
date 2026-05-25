5e5a6f61529ef0c056005c5c5721e0ad92e16e7e
YASEEEN2005 <muhammadyaseen1907@gmail.com>
2026-05-25 21:34:05 +0530

    feat: add responsive WhyValam component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from './components/Navbar'
 import HeroSection from './components/HeroSection'
 import SearchAndOffers from './components/SearchAndOffers'
 import ReadyToDrive from './components/ReadyToDrive'
+import WhyValam from './components/WhyValam'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <HeroSection />
       <SearchAndOffers />
+      <WhyValam />
       <ReadyToDrive />
     </>
   )
